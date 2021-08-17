67d8b729b0dd8131fc794fd790afa2a2d33483b8
primerxiao <primerxiao@163.com>
2021-08-17 20:43:21 +0800

    反向更新字段注释的功能

---
+++ src/main/java/com/efp/plugins/project/coder/ui/ModleCompareWithTable.java
@@ -0,0 +1,12 @@
+package com.efp.plugins.project.coder.ui;
+
+import javax.swing.*;
+
+public class ModleCompareWithTable {
+    private JPanel jPanel;
+    private JTextArea textArea1;
+    private JCheckBox poCheckBox;
+    private JCheckBox outputCheckBox;
+    private JCheckBox inputCheckBox;
+    private JCheckBox doCheckBox;
+}
