250dfac13f45f45fd51d3c366a8bb50242350828
primerxiao <primerxiao@163.com>
2021-10-09 19:07:24 +0800

    Optimize the project structure

--- src/main/java/com/efp/plugins/project/gitmr/ui/GitMrUi.java
+++ src/main/java/com/efp/plugins/project/gitmr/ui/GitMrUi.java
@@ -14,15 +14,12 @@ import com.intellij.ui.JBColor;
 import org.apache.commons.lang3.StringUtils;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
-import org.gitlab4j.api.models.Commit;
+import org.gitlab4j.api.models.CompareResults;
 import org.gitlab4j.api.models.Diff;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.table.TableModel;
-import java.awt.*;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -37,11 +34,6 @@ import java.util.stream.Collectors;
 public class GitMrUi extends DialogWrapper {
     private JTextField gitAccount;
     private JPasswordField gitPassword;
-    private JTextField startTime;
-    private JTextField endTime;
-    private JButton last30MinuteButton;
-    private JButton last2HourButton;
-    private JButton currentDateButton;
     private JComboBox<String> toUserComboBox;
     private JTable appInfoTable;
     private JPanel jPanel;
@@ -53,7 +45,7 @@ public class GitMrUi extends DialogWrapper {
 
     private Project project;
 
-    private static final List<CommitInfo> commitInfos = new ArrayList<>();
+    private static final List<GitProjectInfo> diffGitProject = new ArrayList<>();
 
     public GitMrUi(@Nullable Project project) {
         super(project);
@@ -85,31 +77,6 @@ public class GitMrUi extends DialogWrapper {
             String key = stringStringEntry.getKey();
             toUserComboBox.addItem(key);
         }
-        //按钮事件
-        last30MinuteButton.addActionListener(e -> {
-            LocalDateTime now = LocalDateTime.now();
-            String nowFormart = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            LocalDateTime pre = now.plusMinutes(-30);
-            String preFormart = pre.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            startTime.setText(preFormart);
-            endTime.setText(nowFormart);
-        });
-        last2HourButton.addActionListener(e -> {
-            LocalDateTime now = LocalDateTime.now();
-            String nowFormart = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            LocalDateTime pre = now.plusHours(-2);
-            String preFormart = pre.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            startTime.setText(preFormart);
-            endTime.setText(nowFormart);
-        });
-        currentDateButton.addActionListener(e -> {
-            LocalDateTime now = LocalDateTime.now();
-            String nowFormart = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            LocalDate nowDate = LocalDate.now();
-            String nowDateFormat = nowDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            startTime.setText(nowDateFormat + " 00:00:00");
-            endTime.setText(nowFormart);
-        });
         fetchCommitInfoButton.addActionListener(e -> {
             console.setText("");
             //登陆gitlab
@@ -166,13 +133,9 @@ public class GitMrUi extends DialogWrapper {
     private boolean paramCheck() {
         String gitAccountText = gitAccount.getText();
         String gitPasswordText = gitPassword.getText();
-        String startTimeText = startTime.getText();
-        String endTimeText = endTime.getText();
 
         if (StringUtils.isEmpty(gitAccountText) ||
-                StringUtils.isEmpty(gitPasswordText) ||
-                StringUtils.isEmpty(startTimeText) ||
-                StringUtils.isEmpty(endTimeText)
+                StringUtils.isEmpty(gitPasswordText)
         ) {
             NotificationHelper.getInstance().notifyError("参数不能为空", project);
             return false;
@@ -188,31 +151,28 @@ public class GitMrUi extends DialogWrapper {
 
     public void createMrRequest(GitLabApi gitLabApi) throws GitLabApiException {
         //如果数据为空不做处理
-        if (commitInfos.isEmpty()) {
-            NotificationHelper.getInstance().notifyError("数据为空或者未进行提交信息校对", project);
-            console.append("数据为空或者未进行提交信息校对" + "\r\n");
+        if (diffGitProject.isEmpty()) {
+            NotificationHelper.getInstance().notifyError("差异数据为空或者未进行提交信息校对", project);
+            console.append("差异数据为空或者未进行提交信息校对" + "\r\n");
             return;
         }
-        if (appInfoTable.getSelectedRows().length < 1) {
-            NotificationHelper.getInstance().notifyError("未选择项目", project);
-            console.append("未选择项目" + "\r\n");
-            return;
-        }
-
-
-        List<AppInfo> currentSelectList = new ArrayList<>();
+        int[] selectedRows = appInfoTable.getSelectedRows();
+        List<String> currentSelectList = new ArrayList<>();
         AppInfoTableModel appInfoTableModel = (AppInfoTableModel) appInfoTable.getModel();
         List<AppInfo> appInfoList = appInfoTableModel.getAppInfoList();
-        int[] selectedRows = appInfoTable.getSelectedRows();
         for (int selectedRow : selectedRows) {
-            currentSelectList.add(appInfoList.get(selectedRow));
+            AppInfo appInfo = appInfoList.get(selectedRow);
+            if (!appInfo.getCompareStatus().equals("有差异")) {
+                continue;
+            }
+            currentSelectList.add(appInfo.getProjectId());
         }
-        for (CommitInfo commitInfo : commitInfos) {
-            //提交合并请求
-            //判断是否被选择
-            AppInfo check = currentSelectList.stream().filter(appInfo -> appInfo.getProjectId().equals(commitInfo.getProjectId())).findFirst().orElse(null);
-            if (check != null) {
-                return;
+        for (GitProjectInfo commitInfo : diffGitProject) {
+            //判断是否被选择 没选择 默认提交所有差异
+            if (selectedRows.length >= 1) {
+                if (!currentSelectList.contains(commitInfo.getProjectId())) {
+                    continue;
+                }
             }
             NotificationHelper.getInstance().notifyError("开始处理模块[" + commitInfo.getProjectName() + "]代码合并-----------", project);
             console.append("开始处理模块[" + commitInfo.getProjectName() + "]代码合并-----------" + "\r\n");
@@ -222,7 +182,7 @@ public class GitMrUi extends DialogWrapper {
                         GitMrUtils.getBranchName(1, GitMrUtils.getProjectInfoById(commitInfo.getProjectId())),
                         GitMrUtils.getBranchName(2, GitMrUtils.getProjectInfoById(commitInfo.getProjectId())),
                         "【s1-》uat】【账号：" + gitAccount.getText() + " 时间：" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + "】",
-                        commitInfo.getCommitMessages().stream().collect(Collectors.joining("/r/n")),
+                        "代码合并申请",
                         getAssignIdBySelect(),
                         Integer.valueOf(commitInfo.getProjectId()),
                         new String[]{},
@@ -240,58 +200,23 @@ public class GitMrUi extends DialogWrapper {
     }
 
     public void setCommitInfo(GitLabApi gitLabApi) throws GitLabApiException, ParseException {
-        commitInfos.clear();
+        diffGitProject.clear();
         AppInfoTableModel tableModel = (AppInfoTableModel) appInfoTable.getModel();
         List<AppInfo> appInfoList = tableModel.getAppInfoList();
         //获取配置的项目
         for (AppInfo appInfo : appInfoList) {
             //s1和uat分支比较是否有差异
-            List<Commit> commits = gitLabApi.getCommitsApi().getCommits(
-                    appInfo.getProjectId(),
-                    GitMrUtils.getBranchName(1, GitMrUtils.getProjectInfoById(appInfo.getProjectId())),
-                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(startTime.getText()),
-                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(endTime.getText()));
-            
-            if (commits.isEmpty()) {
+            CompareResults compare = gitLabApi.getRepositoryApi().compare(appInfo.getProjectId(), GitMrUtils.getBranchName(1, GitMrUtils.getProjectInfoById(appInfo.getProjectId())), GitMrUtils.getBranchName(2, GitMrUtils.getProjectInfoById(appInfo.getProjectId())), true);
+            List<Diff> diffs = compare.getDiffs();
+
+            if (diffs.isEmpty()) {
                 appInfo.setCompareStatus("无差异");
                 continue;
             }
             appInfo.setCompareStatus("有差异");
-            //其它工程
-            CommitInfo commitInfo = new CommitInfo();
-            commitInfo.setCommitFiles(new ArrayList<>());
-            commitInfo.setCommitMessages(new ArrayList<>());
-            commitInfo.setCommits(new ArrayList<>());
-            //设置项目信息
-            commitInfo.setProjectId(appInfo.getProjectId());
-            commitInfo.setProjectName(appInfo.getProjectName());
-            for (Commit commit : commits) {
-//                if (gitAccount.getText().equals(commit.getCommitterName())) {
-                String message = commit.getMessage();
-                if (message.startsWith("Merge")) {
-                    continue;
-                }
-                //设置提交信息
-                if (!commitInfo.getCommitMessages().contains(message)) {
-                    commitInfo.getCommitMessages().add(message);
-                }
-                //设置提交文件清单
-                List<Diff> diffs = gitLabApi.getCommitsApi().getDiff(appInfo.getProjectId(), commit.getId());
-                for (Diff diff : diffs) {
-                    if (!commitInfo.getCommitFiles().contains(diff.getOldPath())) {
-                        commitInfo.getCommitFiles().add(diff.getOldPath());
-                    }
-                    if (!commitInfo.getCommitFiles().contains(diff.getNewPath())) {
-                        commitInfo.getCommitFiles().add(diff.getNewPath());
-                    }
-                }
-//                }
-            }
-            if (!commitInfo.getCommitFiles().isEmpty()) {
-                commitInfos.add(commitInfo);
-            }
-            tableModel.fireTableDataChanged();
+            diffGitProject.add(GitMrUtils.getProjectInfoById(appInfo.getProjectId()));
         }
+        tableModel.fireTableDataChanged();
         gitLabApi.close();
     }
 
@@ -317,15 +242,11 @@ public class GitMrUi extends DialogWrapper {
     private void initConfig() {
         gitAccount.setText(PropertiesComponent.getInstance(project).getValue("GIT_MR_CACH_EKEY_1"));
         gitPassword.setText(PropertiesComponent.getInstance(project).getValue("GIT_MR_CACH_EKEY_2"));
-        startTime.setText(PropertiesComponent.getInstance(project).getValue("GIT_MR_CACH_EKEY_3"));
-        endTime.setText(PropertiesComponent.getInstance(project).getValue("GIT_MR_CACH_EKEY_4"));
     }
 
     private void cacheConfig() {
         PropertiesComponent.getInstance(project).setValue("GIT_MR_CACH_EKEY_1", gitAccount.getText());
         PropertiesComponent.getInstance(project).setValue("GIT_MR_CACH_EKEY_2", gitPassword.getText());
-        PropertiesComponent.getInstance(project).setValue("GIT_MR_CACH_EKEY_3", startTime.getText());
-        PropertiesComponent.getInstance(project).setValue("GIT_MR_CACH_EKEY_4", endTime.getText());
     }
 
 }
